ea468acc98eeaa454604e0e2a70a7571c603e2ae
anahlewi <anah9848@gmail.com>
2026-07-05 15:13:25 -0400

    adding crystals data

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // API Endpoint to read and return the JSON file
-app.get('/api/data', async (req, res) => {
+app.get('/api/affirmations/', async (req, res) => {
   try {
     const filePath = new URL('./affirmations.json', import.meta.url);
     const fileData = await fs.readFile(filePath, 'utf-8');
@@ -24,7 +24,7 @@ app.get('/api/data', async (req, res) => {
   }
 });
 
-app.get('/api/random', async (req, res) => {
+app.get('/api/affirmations/random', async (req, res) => {
     try {
         const filePath = new URL('./affirmations.json', import.meta.url);
         const fileData = await fs.readFile(filePath, 'utf-8');
@@ -37,7 +37,36 @@ app.get('/api/random', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/api/crystals/', async (req, res) => {
+  try {
+    const filePath = new URL('./crystals.json', import.meta.url);
+    const fileData = await fs.readFile(filePath, 'utf-8');
+    const jsonObject = JSON.parse(fileData);
+
+    return res.status(200).json(jsonObject);
+    
+  } catch (error) {
+    // Handle cases where file does not exist or has bad syntax
+    return res.status(500).json({ error: 'Failed to read data file.' });
+  }
+});
+
+app.get('/api/crystals/random', async (req, res) => {
+  try {
+    const filePath = new URL('./crystals.json', import.meta.url);
+    const fileData = await fs.readFile(filePath, 'utf-8');
+    const jsonObject = JSON.parse(fileData);
+    const crystals = jsonObject.crystals;
+    const randomCrystal = crystals[Math.floor(Math.random() * crystals.length)];
+    return res.status(200).json(randomCrystal);
+    
+  } catch (error) {
+    // Handle cases where file does not exist or has bad syntax
+    return res.status(500).json({ error: 'Failed to read data file.' });
+  }
+});
+
+
 
 // Start the server
 app.listen(PORT, () => {
